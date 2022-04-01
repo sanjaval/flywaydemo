6160eee819c4b5f60b7fb0bf67c276422d45e503
sanjay.choubey <sanjay.choubey@avalara.com>
2022-04-01 18:21:45 +0530

    added rollback workflow

---
+++ rollback-test-prod.sh.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+ set -e
+
+ echo
+ echo "Starting migration of database with flyway"
+
+ docker run --net=host --rm -v $PWD/sql:/flyway/sql flyway/flyway undo -url="jdbc:postgresql://rgba562s.instances.spawn.cc:30006/postgres" -user=spawn_admin_YOsj -password=m9lwOA7VeNLA0Qao
+
+ echo "Successfully migrated database"
+ echo
