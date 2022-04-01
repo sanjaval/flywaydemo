9119320b0301e43743cac27135debebec48ce6f4
skctek <102790834+skctek@users.noreply.github.com>
2022-04-01 15:05:07 +0530

    Update migration-test-prod.sh

--- migration-test-prod.sh
+++ migration-test-prod.sh
@@ -5,7 +5,7 @@
  echo
  echo "Starting migration of database with flyway"
 
- docker run --net=host --rm -v $PWD/sql:/flyway/sql flyway/flyway migrate -url="jdbc:postgresql://rgba562s.instances.spawn.cc:30006/postgres" -user=spawn_admin_YOsj -password=m9lwOA7VeNLA0Qao
+ docker run --net=host --rm -v $PWD/sql:/flyway/sql flyway/flyway migrate -url="jdbc:postgresql://rgba562s.instances.spawn.cc:30072/postgres" -user=spawn_admin_YOsj -password=Kjqfw6KNc4trOD5S
 
  echo "Successfully migrated database"
  echo
