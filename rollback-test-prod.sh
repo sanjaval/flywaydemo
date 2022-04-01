3c3796b39a87f952ec9b6b213a84f1ec1a0f2c3e
sanjaval <102798162+sanjaval@users.noreply.github.com>
2022-04-01 18:29:08 +0530

    Update rollback-test-prod.sh

--- rollback-test-prod.sh
+++ rollback-test-prod.sh
@@ -2,10 +2,24 @@
 
  set -e
 
+ echo "Downloading and installing spawnctl..."
+ curl -sL https://run.spawn.cc/install | sh > /dev/null 2>&1
+ export PATH=$HOME/.spawnctl/bin:$PATH
+ echo "spawnctl successfully installed"
+ 
+ databaseName="postgres"
+ pagilaHost="rgba562s.instances.spawn.cc"
+ pagilaPort="30072"
+ pagilaUser="spawn_admin_YOsj"
+ pagilaPassword="Kjqfw6KNc4trOD5S"
+
+ docker pull postgres:12-alpine > /dev/null 2>&1
+ docker pull flyway/flyway
+
  echo
  echo "Starting migration of database with flyway"
 
- docker run --net=host --rm -v $PWD/sql:/flyway/sql flyway/flyway undo -url="jdbc:postgresql://rgba562s.instances.spawn.cc:30006/postgres" -user=spawn_admin_YOsj -password=m9lwOA7VeNLA0Qao
+ docker run --rm -v $PWD/sql:/flyway/sql -v $PWD:/flyway/conf flyway/flyway undo -url="jdbc:postgresql://$pagilaHost:$pagilaPort/$databaseName" -user=$pagilaUser -password=$pagilaPassword
 
- echo "Successfully migrated database"
+ echo "Successfully migrated 'Pagila' database"
  echo
